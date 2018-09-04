cbb2f68acb6e4d39ef0ec34c08b00fa9d68e7710
chris <chris.j.h.original@gmail.com>
2018-09-04 23:04:48 +0100

    Install installs it fully

--- LinuxInstall.sh
+++ LinuxInstall.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+sudo apt install python3-pip python3-argcomplete
+sudo -H pip3 install pypodcastparser
 sudo cp podcast_dl.py /usr/local/bin/podcast-dl
 cd /usr/local/bin
 sudo chmod +x podcast-dl
